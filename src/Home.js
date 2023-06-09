55fde44e39ac9f920bb6d705d4dda4f1116c2184
tua <soumavabiswas15@gmail.com>
2023-06-09 20:57:17 +0530

    state

--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,18 @@ import React from "react";
 
 // Sample class component
 export default class Home extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            data: 'Soumava Biswas'
+        }
+    }
     render() {
         return (
             <div>
                 <h1>This is Home</h1>
-                <p>Home is made by class Component.</p>
+                <p>Home is made by {this.state.data} using class component</p>
+                <button onClick={() => this.setState({ data: 'Tua' })}>Click Me!</button>
             </div>
         )
     }
